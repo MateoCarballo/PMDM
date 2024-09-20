aa9101a7964ba26844f4ca1378c35d3af56888e6
Mateo <mateocarballo2@gmail.com>
2024-09-20 21:12:01 +0200

    Continuando los codelabs

--- Pruebas_KotlinPlayground.kt
+++ Pruebas_KotlinPlayground.kt
@@ -26,6 +26,42 @@
 
 // fun add(first: Int, second: Int)= first + second
 // fun substract(first: Int, second: Int)= first - second
-    
 
+
+fun main() {
+    val operatingSystem = "Debian"
+    val emailId = "realemail@gmail.com"
+
+    println(displayAlertMessage(operatingSystem, emailId))
+}
+
+fun displayAlertMessage(operatingSystem: String, emailId: String)= "There's a new sign-in request on $operatingSystem for your Google Account $emailId"
+
+// Define your displayAlertMessage() below this line.    
+
+
+fun main() {
+    val firstUserEmailId = "user_one@gmail.com"
+
+    // The following line of code assumes that you named your parameter as emailId.
+    // If you named it differently, feel free to update the name.
+    println(displayAlertMessage(emailId = firstUserEmailId))
+    println()
+
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
+}
+
+fun displayAlertMessage(operatingSystem = "Unknown", emailId: String)= "There's a new sign-in request on $operatingSystem for your Google Account $emailId"
+
+// Define your displayAlertMessage() below this line.
 
