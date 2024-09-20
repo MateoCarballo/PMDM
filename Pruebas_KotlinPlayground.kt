eab024bf992b6a6d18d6f69b4f9526b9ad9a2395
Mateo <mateocarballo2@gmail.com>
2024-09-20 21:01:52 +0200

    Primeros pasos en kotlin Playground

---
+++ Pruebas_KotlinPlayground.kt
@@ -0,0 +1,31 @@
+// APARTADO 6
+// fun main() {
+//     val firstNumber = 10
+//     val secondNumber = 5
+
+//     println("$firstNumber + $secondNumber = ${firstNumber+secondNumber}")
+// }
+
+
+// APARTAADO 7
+// fun main() {
+    
+//     val firstNumber = 10
+//     val secondNumber = 5
+//     val thirdNumber = 8   
+
+//     println("$firstNumber + $secondNumber = ${add(firstNumber, secondNumber)}")
+//     println("$firstNumber + $thirdNumber = ${add(firstNumber, thirdNumber)}")
+       
+//     println("$firstNumber + $secondNumber = ${substract(firstNumber, secondNumber)}")
+//     println("$firstNumber + $thirdNumber = ${substract(firstNumber, thirdNumber)}")
+// }
+
+
+// // Define add() function below this line
+
+// fun add(first: Int, second: Int)= first + second
+// fun substract(first: Int, second: Int)= first - second
+    
+
+
