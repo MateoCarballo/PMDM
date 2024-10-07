816e89b87b394b41e7f6b50efc7eccd888050cd6
Mateo <mateocarballo2@gmail.com>
2024-10-07 21:21:30 +0200

    Completada segunda parte del codelab tarjeta de felicitacion

--- Codelabs/DisenhoBasico/app/src/main/java/com/codelabs/primeraapp/MainActivity.kt
+++ Codelabs/DisenhoBasico/app/src/main/java/com/codelabs/primeraapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.codelabs.primeraapp
 
+import androidx.compose.foundation.Image
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -15,6 +18,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,9 +38,14 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    GrettingText(
-                        message = "Feliz Cumple Mateo",
-                        from = "De parte de Angel",
+//                    GreetingText(
+//                        message = "Feliz Cumple Mateo",
+//                        from = "De parte de Angel",
+//                        modifier = Modifier.padding(8.dp)
+//                    )
+                    GreetingImage(
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text),
                         modifier = Modifier.padding(8.dp)
                     )
                 }
@@ -52,7 +64,7 @@ class MainActivity : ComponentActivity() {
 //}
 
 @Composable
-fun GrettingText(message: String, from: String, modifier: Modifier = Modifier){
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
     Column (
         verticalArrangement = Arrangement.Center,
         modifier = modifier,
@@ -67,10 +79,30 @@ fun GrettingText(message: String, from: String, modifier: Modifier = Modifier){
         Text(
             text = from,
             fontSize = 36.sp,
-            color = Color.LightGray,
+            color = Color.Green,
             modifier = Modifier
                 .padding(16.dp)
                 .align(alignment = Alignment.End),
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier= Modifier){
+    val image = painterResource(R.drawable.androidparty )
+    Box(modifier){
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.55F,
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier= Modifier
+                .fillMaxSize()
+                .padding(8.dp)
         )
     }
 }
@@ -79,7 +111,18 @@ fun GrettingText(message: String, from: String, modifier: Modifier = Modifier){
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GrettingText(message = "Feliz Cumple Mateo!", from = " De parte de Angel")
+        GreetingImage(
+            message = "Feliz Cumple Mateo!",
+            from = " De parte de Angel")
 
     }
 }
+
+//@Preview(showBackground = true)
+//@Composable
+//fun BirthdayCardPreview() {
+//    HappyBirthdayTheme {
+//        GreetingText(message = "Feliz Cumple Mateo!", from = " De parte de Angel")
+//
+//    }
+//}
