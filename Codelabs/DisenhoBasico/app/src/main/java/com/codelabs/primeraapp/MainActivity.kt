86068bbb136d36d5553a69cccbdf182c494c7334
Mateo <mateocarballo2@gmail.com>
2024-10-07 20:50:27 +0200

    Completada primera parte del codelab tarjeta de felicitacion

--- Codelabs/DisenhoBasico/app/src/main/java/com/codelabs/primeraapp/MainActivity.kt
+++ Codelabs/DisenhoBasico/app/src/main/java/com/codelabs/primeraapp/MainActivity.kt
@@ -4,31 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.codelabs.primeraapp.ui.theme.PrimeraAppTheme
+import com.codelabs.primeraapp.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PrimeraAppTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
+            HappyBirthdayTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    GrettingText(
+                        message = "Feliz Cumple Mateo",
+                        from = "De parte de Angel",
+                        modifier = Modifier.padding(8.dp)
+                    )
                 }
+            }
             }
         }
     }
@@ -44,17 +53,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GrettingText(message: String, from: String, modifier: Modifier = Modifier){
-    Column (modifier = modifier) {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier,
+        ) {
         Text(
             text = message,
             fontSize = 100.sp,
             lineHeight = 116.sp,
             color = Color.Magenta,
+            textAlign = TextAlign.Center,
         )
         Text(
             text = from,
             fontSize = 36.sp,
             color = Color.LightGray,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End),
         )
     }
 }
@@ -62,7 +78,7 @@ fun GrettingText(message: String, from: String, modifier: Modifier = Modifier){
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
-    PrimeraAppTheme {
+    HappyBirthdayTheme {
         GrettingText(message = "Feliz Cumple Mateo!", from = " De parte de Angel")
 
     }
