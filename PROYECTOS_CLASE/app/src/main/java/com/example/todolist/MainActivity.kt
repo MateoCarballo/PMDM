3bef9ec83d172c91a954339a5f7fd03b6cba4a27
Mateo <mateocarballo2@gmail.com>
2024-11-08 21:01:37 +0100

    Modificando la aplicacion Pruebalo

--- PROYECTOS_CLASE/app/src/main/java/com/example/todolist/MainActivity.kt
+++ PROYECTOS_CLASE/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.todolist
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
@@ -22,8 +27,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.todolist.ui.theme.TodoListTheme
 
@@ -46,13 +56,22 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Container(modifier: Modifier = Modifier) {
     Column(
-        modifier = Modifier
-            .background(MaterialTheme.colorScheme.surface)
-            .padding(16.dp)
-            .background(MaterialTheme.colorScheme.inverseOnSurface)
+        modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
     ){
-        Row (modifier = modifier){
+        Row (
+            modifier = Modifier
+                .padding(15.dp),
+        ){
+            Text(text = "To-Do List",)
+        }
+
+        Row (modifier = modifier
+            .padding(15.dp),
+            ){
             TextFieldEntradaLista(modifier)
+            Spacer(modifier = Modifier.padding(10.dp))
             ButtonAddItem(modifier)
         }
         LazyColumnList()
@@ -66,19 +85,19 @@ fun TextFieldEntradaLista(modifier: Modifier){
     }
 
     TextField(
+        modifier = Modifier,
         value = campo,
         onValueChange = {campo = it},
-        label = {Text ("Elemento")},
         placeholder = {Text ("Añade un nuevo elemento a la lista")}
     )
 }
 
 @Composable
-fun ButtonAddItem(
-    modifier: Modifier
-) {
+fun ButtonAddItem(modifier: Modifier) {
     IconButton(
-        modifier = modifier,
+        modifier = modifier
+            .clip(CircleShape)
+            .background(MaterialTheme.colorScheme.primary),
         onClick = {},
         content = {
             Icon(Icons.Default.Add, contentDescription = "Add button")
@@ -91,7 +110,7 @@ fun LazyColumnList() {
 
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     TodoListTheme {
