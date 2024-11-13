f36e4fbd19cb3e4059b9bb969ad7bc4951d3e702
Mateo <mateocarballo2@gmail.com>
2024-11-13 21:01:38 +0100

    .

--- PROYECTOS_CLASE/app/src/main/java/com/example/todolist/MainActivity.kt
+++ PROYECTOS_CLASE/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.todolist
 
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -30,10 +28,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.todolist.ui.theme.TodoListTheme
 
