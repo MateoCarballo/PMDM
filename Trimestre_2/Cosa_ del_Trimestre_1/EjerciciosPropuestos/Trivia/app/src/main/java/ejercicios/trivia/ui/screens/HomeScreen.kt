add10d338330799c1acac8d719a69688821b7ec5
MateoPortatil <mateocarballo2@gmail.com>
2025-03-10 17:46:37 +0100

    Empezando a plantear la pantalla de inicio

--- Trimestre_2/Cosa_ del_Trimestre_1/EjerciciosPropuestos/Trivia/app/src/main/java/ejercicios/trivia/ui/screens/HomeScreen.kt
+++ Trimestre_2/Cosa_ del_Trimestre_1/EjerciciosPropuestos/Trivia/app/src/main/java/ejercicios/trivia/ui/screens/HomeScreen.kt
@@ -1,11 +1,27 @@
 package ejercicios.trivia.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import ejercicios.trivia.navigation.AppScreens
 
 @Composable
 fun HomeScreen(
     toGameScreen: () -> Unit,
 ) {
+    Column(
 
+    ) {
+        Text("Bienvenido a Trivia App")
+        Spacer(modifier = Modifier.padding(vertical = 16.dp))
+        Button(
+            //TODO Pendiente de configurar el sumar una a las preguntas totales con las que jugaremos
+            onClick = {}
+        ) { }
+    }
 }
