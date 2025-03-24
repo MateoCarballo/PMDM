fb671262dd57dfb6403d66535565c1a28cf37e90
Mateo <mateocarballo2@gmail.com>
2025-03-24 13:53:45 +0100

    Creado paquete data para usar Room

---
+++ ExamenPrimerTrimestre/app/src/main/java/com/codelabs/examenprimertrimestre/data/AppContainer.kt
@@ -0,0 +1,13 @@
+package com.codelabs.examenprimertrimestre.data
+
+import android.content.Context
+
+class AppContainer(context: Context) {
+    private val appDatabase = AppDatabase.getDatabase(context)
+    private val productDao = appDatabase.productDao()
+    val productRepository = ProductRepositoryImpl(productDao)
+
+    fun provideProductRepository(): ProductRepository{
+        return productRepository
+    }
+}
